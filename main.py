94b62ba25abe6e19b5d398fcd30d499ccfbdee57
victorsch <victor.schiavo52@yahoo.com>
2018-06-17 01:24:35 -0400

    Main author detection program

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
-from __future__ import division
 import nltk
 from nltk import word_tokenize
+from nltk.util import ngrams
 from collections import Counter
 
-text = open("happy.txt").read()
-token = word_tokenize(text)
-text2 = open("angry.txt").read()
-token2 = word_tokenize(text2)
+textone = open("input/one.txt").read()
+texttwo = open("input/two.txt").read()
 
+tokenone = word_tokenize(textone)
+tokentwo = word_tokenize(texttwo)
 
-def ngram():
-    new = []
-    for x in range(0,len(token) - 2):
-        new.append(token[x] + ',' + token[x+1] + ',' + token[x+2] + ',')
-    return(new)
-    
-def ngram2():
-    new = []
-    for x in range(0,len(token2) - 2):
-        new.append(token2[x] + ',' + token2[x+1] + ',' + token2[x+2] + ',')
-    return(new)
+trigramone = ngrams(tokenone,3)
+trigramtwo = ngrams(tokentwo, 3)
 
-    
-ngram()
-ngram2()
-x = Counter(ngram())
-y = Counter(ngram2())
-
-#print(list(set(ngram()).intersection(ngram2())))
-pct_similarity = (len(list(set(ngram()).intersection(ngram2()))))/len(ngram2())
-print(pct_similarity)
-print(len(ngram2()))
-
+print Counter(trigramone)
+print Counter(trigramtwo)
